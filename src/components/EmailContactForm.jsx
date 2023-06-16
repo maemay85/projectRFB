436bf13b763282f1e407169bc3fc324099dff1a0
maemay85 <hellomaemay@gmail.com>
2023-06-16 17:37:12 -0400

    first attempt to fix email template

--- src/components/EmailContactForm.jsx
+++ src/components/EmailContactForm.jsx
@@ -13,20 +13,24 @@ const EmailContactForm = () => {
 
     emailjs.sendForm('service_f0weiij', 'template_9tf6q6f', form.current, 'hxHheb-8UKemaGtn8').then((result) => {
       console.log(result.text);
+      alert("message sent!")
     }, (error) => {
       console.log(error.text);
     })
+    e.target.reset()
   }
 
   return (
     <>
       <form ref={form} onSubmit={sendEmail}>
-        <label>Name:</label>
-        <input type="text" name="user_name" />
-        <label>Email:</label>
-        <input type="email" name="user_email" />
-        <label>Message:</label>
-        <textarea name="message" />
+
+        <input type="text" placeholder="Name" name="name" />
+
+        <input type="email" placeholder="Email" name="email" />
+
+        <input type="text" placeholder="Subject" name="subject" />
+
+        <textarea placeholder="Message" name="message" />
         <input type="submit" value="Send" className="submit" />
       </form>
       <SpaceLink>
