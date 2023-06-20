3aa32c9e546778ac7851fe62cd11cb9d555fa651
maemay85 <hellomaemay@gmail.com>
2023-06-20 14:52:29 -0400

    updated contact to have reuben's linkedin and instagram

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,20 +20,16 @@ function Contact() {
               <Link to={'/email'}>Email</Link>
             </ContactStyledLink>
             <ContactStyledLink margin="0 2rem 0 0">
-              <a href="https://www.linkedin.com/in/mae-may/" target='_blank' rel='noreferrer'>
+              <a href="https://www.linkedin.com/in/reuben-fuller-bennett-ab9a0031/" target='_blank' rel='noreferrer'>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg" width="50" />
               </a>
             </ContactStyledLink>
             <ContactStyledLink margin="0 2rem 0 0">
-              <a href="https://www.instagram.com/itsmemaemay/" target='_blank' rel='noreferrer'>
+              <a href="https://www.instagram.com/reuben.esque/" target='_blank' rel='noreferrer'>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/1/13/CIS-A2K_Instagram_Icon_%28Pink%29.svg" width="50" />
               </a>
             </ContactStyledLink>
-            <ContactStyledLink margin="0 2rem 0 0">
-              <a href="https://github.com/maemay85" target='_blank' rel='noreferrer'>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Github-desktop-logo-symbol.svg" width="50" />
-              </a>
-            </ContactStyledLink>
+
 
 
         </RowContainer>
