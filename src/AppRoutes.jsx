525d136318c6346272dd2f7c046e1a0dff7c8156
maemay85 <hellomaemay@gmail.com>
2023-06-20 14:32:47 -0400

    created TenantResources

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home/Home"
 import About from "./components/About/About"
-import Projects from "./components/Projects"
+import TenantResources from "./components/Projects"
 import Contact from "./components/Contact"
 import EmailContactForm from "./components/EmailContactForm"
 
@@ -11,7 +11,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Projects />} />
+        <Route path='/projects' element={<TenantResources />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/email' element={<EmailContactForm />} />
         <Route path='*' element={<Home />} />
---
+++ src/components/TenantResources.jsx
@@ -0,0 +1,51 @@
+import { ColumnContainer, RowContainer } from "./styled-components/Containers"
+import ResourceCard from "./styled-components/ResourceCard"
+
+
+
+const TenantResources = () => {
+  const tenantResources = [
+    {
+      title: 'resource one',
+      tagline: 'A brief description of this resource for tenants',
+      url: 'url of the resource',
+      imageUrl: 'image url for the resource'
+    },
+
+    {
+      title: 'resource two',
+      tagline: 'A brief description of this resource for tenants',
+      url: 'url of the resource',
+      imageUrl: 'image url for the resource'
+    },
+    {
+      title: 'resource three',
+      tagline: 'A brief description of this resource for tenants',
+      url: 'url of the resource',
+      imageUrl: 'image url for the resource'
+    },
+  ]
+
+
+  return (
+    <>
+      <ColumnContainer width='100vw'>
+        {tenantResources.map((resource)=>{
+          return (
+            <ResourceCard
+              key={resource.title}
+              text={resource.title}
+              url={resource.url}
+              tagline={resource.tagline}
+              imageUrl={resource.imageUrl}
+            />
+          )
+        })
+
+        }
+      </ColumnContainer>
+    </>
+  )
+}
+
+export default TenantResources
