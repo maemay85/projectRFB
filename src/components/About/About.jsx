a04efbc0715b4887782f705bf77566a4837ce7dd
maemay85 <hellomaemay@gmail.com>
2023-08-16 17:16:18 -0400

    testing image src

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,7 @@ const language = props.language
           </div>
           <div id="about-page-content-container">
             <div id="about-page-headshot-image">
-            <img src="src/assets/headshot.png" />
+            <img src="https://github.com/maemay85/projectRFB/blob/main/src/assets/headshot.png" />
           </div>
           <div id="about-page-text-wrapper">
             {language==='english' ?
