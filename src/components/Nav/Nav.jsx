edb65fa8adfaec0a67cd4ea908ea5ddc3a76d967
maemay85 <hellomaemay@gmail.com>
2023-10-17 12:50:49 -0400

    some style tweaks, IMPORTANT- changed html font-size to .6vw, so that rem units are equivalent to Figma

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -21,27 +21,30 @@ function Nav(props) {
           </div>
           <div id="site-title-text" className='column'>Law Firm of<br/>Reuben Fuller-Bennett</div>
         </Link>
+        <div id="nav-right-side-container">
 
-        <div id="nav-link-wrapper" className='row'>
-          <Link to={'/resources'}>{language === 'english' ? 'RESOURCES' : 'RECURSOS'}</Link>
+           <div id="nav-language-toggle">
+            <span>
+              <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/language.svg" id="language-icon"/>
+            </span>
+            <span className={language==='english'? 'selected' : null} onClick={(e) => handleLanguageChange(e)}>ENGLISH</span>
+            <span className={language==='español'? 'selected' : null} onClick={(e) => handleLanguageChange(e)}>ESPAÑOL</span>
+          </div>
 
-          <Link to={'/about'}>{language === 'english' ? 'ABOUT' : 'ACERCA'}</Link>
+          <div id="nav-link-wrapper">
+            <Link to={'/resources'}>{language === 'english' ? 'RESOURCES' : 'RECURSOS'}</Link>
 
-          {/* <Link to={'/blog'}>BLOG</Link> */}
+            <Link to={'/about'}>{language === 'english' ? 'ABOUT' : 'ACERCA'}</Link>
 
-          <Link to={'/contact'}>{language === 'english' ? 'CONTACT' : 'CONTACTOS'}</Link>
+            {/* <Link to={'/blog'}>BLOG</Link> */}
 
-          {/* <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/search.svg" id="search-icon"/> */}
+            <Link to={'/contact'}>{language === 'english' ? 'CONTACT' : 'CONTACTOS'}</Link>
+
+            {/* <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/search.svg" id="search-icon"/> */}
+          </div>
+
+
         </div>
-
-        <div id="nav-language-toggle" className='row'>
-          <span>
-            <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/language.svg" id="language-icon"/>
-          </span>
-          <span className={language==='english'? 'selected' : null} onClick={(e) => handleLanguageChange(e)}>ENGLISH</span>|
-          <span className={language==='español'? 'selected' : null} onClick={(e) => handleLanguageChange(e)}>ESPAÑOL</span>
-        </div>
-
       </div>
 
     </>
