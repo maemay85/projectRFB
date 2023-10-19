280abffae75c9ffc3414fd7217df82c1443ef1c8
maemay85 <hellomaemay@gmail.com>
2023-10-19 11:03:32 -0400

    added Arrow svg to assets

--- src/components/SectionDivider/SectionDivider.jsx
+++ src/components/SectionDivider/SectionDivider.jsx
@@ -8,27 +8,42 @@ const SectionDivider = (props) => {
 
   return (
     <>
-      {language === 'english' ?
-        <div className="page-description-container column">
           {section === 'resources' ?
-            <>
-              <h2>EXPLORE RESOURCES</h2>
-              <p>Here you will find resources, organizations and tools  to support you in understanding and advocating for your rights. </p>
-            </>
+            <div id='explore-resources'>
+              <div className="section-divider column">
+                {language === 'english' ?
+                  <>
+                    <h2>EXPLORE RESOURCES</h2>
+                    <p>Here you will find resources about protecting the housing rights of tenants in New York City. There is a curated list of organizations, websites, and databases which will support you in understanding and advocating for your rights.</p>
+                  </>
+                  :
+                  <>
+                    <h2>EXPLORAR RECURSOS</h2>
+                    <p>{`Aquí encontrará recursos sobre cómo proteger los derechos de vivienda de los inquilinos en la ciudad de Nueva York. Existe una lista seleccionada de organizaciones, sitios web y bases de datos que le ayudarán a comprender y defender sus derechos.`}</p>
+                  </>
+                }
+                <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/Arrow.svg" />
+              </div>
+            </div>
             :
-            <>
-              <h2>LEARN MORE</h2>
-              <p>{`Read more about Reuben's practice`}</p>
-            </>
+            <div id='learn-more'>
+              <div className='section-divider column'>
+                {language === 'english' ?
+                  <>
+                    <h2>LEARN MORE</h2>
+                    <p>{`Read more about Reuben's practice`}</p>
+                  </>
+                  :
+                  <>
+                    <h2>{`APRENDE MÁS`}</h2>
+                    <p>{`Leer más sobre la práctica de Reuben`}</p>
+                  </>
+                }
+                <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/Arrow.svg" />
+              </div>
+            </div>
 
           }
-      </div>
-        :
-        <div className="page-description-container column">
-          <h2>EXPLORAR RECURSOS</h2>
-          <p>Aquí encontrará recursos, organizaciones y herramientas que le ayudarán a comprender y defender sus derechos. </p>
-        </div>
-      }
     </>
   )
 }
--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -6,23 +6,22 @@ const Top = (props) => {
   return (
     <>
       <div id="top-container" className="column">
+      <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/light_gradient map 1.png" />
         {language === 'english' ?
-          <div id="top-wrapper" className="column">
-
-            <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/light_gradient map 1.png" />
 
 
 
-            <div id='top-text-top-lines'><h1>You have rights at home,<br/>but it’s up to you to enforce them. </h1></div>
+
+          <div id="top-text-wrapper" className="column">
+
+            <div id='top-text-top-lines'><h2>You have rights at home,<br/>but it’s up to you to enforce them. </h2></div>
             <div id='top-text-bottom-line'><h1>Reuben can help.</h1></div>
 
           </div>
           :
-          <div id="top-wrapper" className="column">
+          <div id="top-text-wrapper" className="column">
 
-            <img src="https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/light_gradient map 1.png" />
-
-            <div id='top-text-top-lines'><h1>Tienes derechos en casa, <br/>pero depende de ti hacerlos cumplir. </h1></div>
+            <div id='top-text-top-lines'><h2>Tienes derechos en casa, <br/>pero depende de ti hacerlos cumplir. </h2></div>
             <div id='top-text-bottom-line'><h1>Reuben puede ayudar.</h1></div>
 
           </div>
