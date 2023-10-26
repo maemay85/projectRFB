616ccf6d9916bf219cbf9e270184fe90e7bd3d6b
maemay85 <hellomaemay@gmail.com>
2023-10-26 12:24:38 -0400

    populated Resources with descriptions written by ChatGPT and translated to Espanol. Added urls to external websites

--- src/assets/resourceData.js
+++ src/assets/resourceData.js
@@ -3,41 +3,77 @@ const resourceData =
         {
           title: {
                    english: 'Automated City Register Information System (ACRIS)',
-                   español: 'Recurso Uno'
+                   español: 'Sistema Automatizado de Información del Registro Municipal (ACRIS)'
                  },
           body:  {
-                   english: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat',
-                   español: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat...'
+                   english: 'The Automated City Register Information System (ACRIS) allows you to search property records and view document images for Manhattan, Queens, Bronx, and Brooklyn from 1966 to the present',
+                   español: 'El Sistema Automatizado de Información del Registro Municipal (ACRIS) le permite buscar registros de propiedad y ver imágenes de documentos de Manhattan, Queens, Bronx y Brooklyn desde 1966 hasta el presente'
                  },
-          url: 'url of the resource',
-          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/Resource 1.png'
+          url: 'https://a836-acris.nyc.gov/CP/',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/PermitsAptApp.png'
         },
 
         {
           title: {
-            english: 'Resource Two',
-            español: 'Recurso Dos'
+            english: `New York City Department of Housing Preservation and Development
+            (HPD)`,
+            español: `Departamento de Preservación y Desarrollo de Vivienda de la Ciudad de Nueva York (HPD)`
           },
-   body:  {
-            english: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat...',
-            español: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat...'
+          body:  {
+            english: 'HPD is a crucial agency established in 1978, focused on preserving and expanding affordable housing. HPD enforces housing codes, supports developers, and administers affordable housing lotteries, aiming to ensure safe, decent, and reasonably priced housing for New Yorkers',
+            español: 'El HPD es una agencia crucial establecida en 1978, enfocada en preservar y expandir viviendas asequibles. El HPD hace cumplir los códigos de vivienda, apoya a los desarrolladores y administra loterías de viviendas asequibles, con el objetivo de garantizar viviendas seguras, decentes y a precios razonables para los neoyorquinos'
           },
-          url: 'url of the resource',
-          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/Resource 2.png'
+          url: 'https://hpdonline.nyc.gov/hpdonline/',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/HPD.png'
         },
         {
           title: {
-            english: 'Resource Three',
-            español: 'Recurso Tres'
+            english: 'The Real Estate Finance Bureau',
+            español: 'La Oficina de Finanzas Inmobiliarias'
           },
    body:  {
-            english: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat...',
-            español: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo csdvfsdvdxvxcvxvxcvxcvxcvxcvxcvxcvxcvxcxconsequat...'
+            english: `The NY Attorney General's Office Real Estate Finance Bureau oversees real estate offerings, ensuring compliance with state laws. It reviews and approves offering plans for cooperative and condominium projects, safeguarding the interests of buyers and investors in New York's real estate market`,
+            español: `La Oficina de Financiamiento de Bienes Raíces de la Oficina del Fiscal General de Nueva York supervisa las ofertas de bienes raíces y garantiza el cumplimiento de las leyes estatales. Revisa y aprueba planes de oferta para proyectos de cooperativas y condominios, salvaguardando los intereses de compradores e inversionistas en el mercado inmobiliario de Nueva York`
           },
-          url: 'url of the resource',
-          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/Resource 3.png'
+          url: 'https://ag.ny.gov/about/about-office/social-justice-division#real-estate-finance',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/FinanceBureau.png'
         },
-
+        {
+          title: {
+            english: `Building Information Search (NYC Department of Buildings)`,
+            español: `Búsqueda de información sobre edificios (Departamento de Edificios de la Ciudad de Nueva York)`
+          },
+          body: {
+            english: `The Building Information Search, provided by the NYC Department of Buildings, is an online tool for accessing crucial information about buildings in New York City. It allows users to retrieve details on property records, permits, violations, and complaints, aiding in transparency and safety in the city's built environment`,
+            español: `La Búsqueda de información sobre edificios, proporcionada por el Departamento de Edificios de la Ciudad de Nueva York, es una herramienta en línea para acceder a información crucial sobre los edificios de la ciudad de Nueva York. Permite a los usuarios recuperar detalles sobre registros de propiedad, permisos, infracciones y quejas, lo que contribuye a la transparencia y la seguridad en el entorno construido de la ciudad`
+          },
+          url: 'https://a810-bisweb.nyc.gov/bisweb/bispi00.jsp',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/BuildingSearch.png'
+        },
+        {
+          title: {
+            english: `Self-service for online Permits, Appointments and Applications (NYC Department of Buildings)`,
+            español: `Autoservicio para permisos, citas y solicitudes en línea (Departamento de Edificios de la Ciudad de Nueva York)`
+          },
+          body: {
+            english: `The Self-service for online Permits, Appointments, and Applications is a convenient digital platform offered by the NYC Department of Buildings. It enables users to easily apply for permits, schedule appointments, and submit various applications related to building and construction projects online, streamlining the process for property owners and developers`,
+            español: `El Autoservicio para Permisos, Citas y Solicitudes en línea es una conveniente plataforma digital ofrecida por el Departamento de Edificios de la Ciudad de Nueva York. Permite a los usuarios solicitar permisos, programar citas y enviar diversas solicitudes relacionadas con proyectos de edificación y construcción en línea fácilmente, agilizando el proceso para propietarios y desarrolladores`
+          },
+          url: 'https://a810-dobnow.nyc.gov/publish/#!/',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/ACRIS.png'
+        },
+        {
+          title: {
+            english: `New York State Unified Court System: eCourts`,
+            español: `Sistema judicial unificado del estado de Nueva York: tribunales electrónicos`
+          },
+          body: {
+            english: `The New York State Unified Court System's eCourts is an advanced digital platform revolutionizing court operations. It allows online access to case information, e-filing of legal documents, and scheduling of court appearances. This user-friendly system enhances efficiency and accessibility in the state's judicial proceedings`,
+            español: `Los tribunales electrónicos del Sistema Judicial Unificado del Estado de Nueva York son una plataforma digital avanzada que revoluciona las operaciones judiciales. Permite el acceso en línea a información del caso, la presentación electrónica de documentos legales y la programación de comparecencias ante el tribunal. Este sistema fácil de usar mejora la eficiencia y accesibilidad en los procedimientos judiciales del estado.`
+          },
+          url: 'https://iapps.courts.state.ny.us/webcivil/ecourtsMain',
+          imageUrl: 'https://raw.githubusercontent.com/maemay85/projectRFB/main/src/assets/eCourts.png'
+        }
       ]
 
 
